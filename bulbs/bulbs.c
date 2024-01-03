4b05b16da7b73e02c42fece281b71d69f9261492
arielmoguillansky <arielmogui92@gmail.com>
2024-01-03 17:43:58 -0300

    added emoji conversor

---
+++ bulbs/bulbs.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+char *convert_to_binary(int decimal);
+
+int main(void)
+{
+    string msg = get_string("Message: ");
+
+    for (int j = 0, len = strlen(msg); j < len; j++)
+    {
+        int binarr[BITS_IN_BYTE];
+        int reversearr[BITS_IN_BYTE];
+
+        for (int i = 0; i < BITS_IN_BYTE; i++)
+        {
+            binarr[i] = (int) msg[j] % 2;
+            msg[j] /= 2;
+        }
+
+        for (int i = 0, n = BITS_IN_BYTE - 1; i <= n; i++)
+        {
+            print_bulb(binarr[n - i]);
+        }
+        printf("\n");
+    }
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
